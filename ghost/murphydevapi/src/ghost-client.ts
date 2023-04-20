43b6a7e50e98accbd7bfae75ca284db71eada210
Jason Murphy <jason.murphy@bbc.co.uk>
2023-04-20 17:47:50 +0100

    Progress

---
+++ ghost/murphydevapi/src/ghost-client.ts
@@ -0,0 +1,7 @@
+import GhostContentAPI from "@tryghost/content-api";
+
+const api = new GhostContentAPI({
+  url: "https://murphydev.digitalpress.blog",
+  key: "644165cbd770a40001f259b9:35e7d3af0bee2a6beca2e77be5f755ef76fb48dce69afc2d2d201d3bf0fea3ee",
+  version: "v3",
+});
