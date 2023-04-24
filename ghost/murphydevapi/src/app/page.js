58666d9b11c2d697d96f001ebd8f3ebbbce1d022
Jason Murphy <jason.murphy@bbc.co.uk>
2023-04-24 10:43:30 +0100

    JS Learning

--- ghost/murphydevapi/src/app/page.js
+++ ghost/murphydevapi/src/app/page.js
@@ -6,7 +6,17 @@ const inter = Inter({ subsets: ["latin"] });
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>MurphyDev Coming Soon</div>
+      <h1>MurphyDev Coming Soon</h1>
+
+      {/* spinner */}
+      <div className="flex items-center justify-center w-24 h-24">
+        <Image
+          src="/spinner.svg"
+          alt="MurphyDev Logo"
+          width={100}
+          height={100}
+        />
+      </div>
     </main>
   );
 }
